ed0f131aeed9eaea4f21774b090d40e7f5f6d37a
Filaret-Niculai Crainiciuc <c.filaret2000@yahoo.com>
2022-05-10 20:00:32 +0300

    Lab8Complet

--- InterfataUtilizator_WindowsForms/Form1.Designer.cs
+++ InterfataUtilizator_WindowsForms/Form1.Designer.cs
@@ -29,21 +29,97 @@ namespace InterfataUtilizator_WindowsForms
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBoxNick = new System.Windows.Forms.TextBox();
+            this.textBoxPct = new System.Windows.Forms.TextBox();
+            this.textBoxData = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // textBoxNick
+            // 
+            this.textBoxNick.Location = new System.Drawing.Point(45, 38);
+            this.textBoxNick.Name = "textBoxNick";
+            this.textBoxNick.Size = new System.Drawing.Size(132, 22);
+            this.textBoxNick.TabIndex = 0;
+            this.textBoxNick.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBoxNick.Click += new System.EventHandler(this.GotFocusNick);
+            this.textBoxNick.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBoxNick.Leave += new System.EventHandler(this.LostFocusNick);
+            // 
+            // textBoxPct
+            // 
+            this.textBoxPct.Location = new System.Drawing.Point(196, 38);
+            this.textBoxPct.Name = "textBoxPct";
+            this.textBoxPct.Size = new System.Drawing.Size(133, 22);
+            this.textBoxPct.TabIndex = 1;
+            this.textBoxPct.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBoxPct.Click += new System.EventHandler(this.GotFocusPct);
+            this.textBoxPct.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBoxPct.Leave += new System.EventHandler(this.LostFocusPct);
+            // 
+            // textBoxData
+            // 
+            this.textBoxData.AccessibleName = "";
+            this.textBoxData.Location = new System.Drawing.Point(347, 38);
+            this.textBoxData.Name = "textBoxData";
+            this.textBoxData.Size = new System.Drawing.Size(133, 22);
+            this.textBoxData.TabIndex = 2;
+            this.textBoxData.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBoxData.Click += new System.EventHandler(this.GotFocusDate);
+            this.textBoxData.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            this.textBoxData.Leave += new System.EventHandler(this.LostFocusDate);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Aquamarine;
+            this.button1.Location = new System.Drawing.Point(499, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 22);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "&Adauga";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.OnButtonAdaugaClick);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Aquamarine;
+            this.button2.Location = new System.Drawing.Point(499, 38);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 22);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Afisea&za";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.OnButtonAfiseazaClick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxData);
+            this.Controls.Add(this.textBoxPct);
+            this.Controls.Add(this.textBoxNick);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.OnFormClick);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxNick;
+        private System.Windows.Forms.TextBox textBoxPct;
+        private System.Windows.Forms.TextBox textBoxData;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- InterfataUtilizator_WindowsForms/Form1.cs
+++ InterfataUtilizator_WindowsForms/Form1.cs
@@ -36,12 +36,17 @@ namespace InterfataUtilizator_WindowsForms
         private Label DateError;
         private Label PctError;
 
+        /* declarare TextBox-uri pentru citire prin cod
         private TextBox TNickname;
         private TextBox TPuncte;
         private TextBox TData;
+        */
 
+        /* declarare butoane adauga si refresh prin cod
         private Button btnAdauga;
         private Button btnRefresh;
+        */
+        private Button btnNext;
 
         public Form1()
         {
@@ -58,8 +63,8 @@ namespace InterfataUtilizator_WindowsForms
 
             //Declarari si initializari
             //initializare forma
-            this.Size = new Size(520,450); 
-            //this.Text = "Snake";
+            this.Size = new Size(480,110); 
+            this.Text = "Snake";
             this.ForeColor = Color.Blue;
             this.Font = new Font("Times New Roman", 12, FontStyle.Bold);
             this.StartPosition = FormStartPosition.CenterScreen;
@@ -68,7 +73,6 @@ namespace InterfataUtilizator_WindowsForms
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.WindowState = FormWindowState.Normal;
-            this.AutoScroll = true;
 
             LNickname = new Label();
             LNickname.Width = LATIME_CONTROL;
@@ -79,11 +83,13 @@ namespace InterfataUtilizator_WindowsForms
             LNickname.TextAlign = ContentAlignment.MiddleCenter;
             this.Controls.Add( LNickname );
 
+            /* TextBoxNick pentru citire definit prin cod
             TNickname = new TextBox();
             TNickname.Width = LATIME_CONTROL;
             TNickname.Top = ALINIERE_Y + DIMENSIUNE_PAS_Y;
             TNickname.Left = ALINIERE_X;
             this.Controls.Add( TNickname );
+            */
 
             LPuncte = new Label();
             LPuncte.Width = LATIME_CONTROL;
@@ -94,11 +100,13 @@ namespace InterfataUtilizator_WindowsForms
             LPuncte.TextAlign = ContentAlignment.MiddleCenter;
             this.Controls.Add( LPuncte );
 
+            /* TextBoxPct pentru citire definit prin cod
             TPuncte = new TextBox();
             TPuncte.Width = LATIME_CONTROL;
             TPuncte.Top = ALINIERE_Y + DIMENSIUNE_PAS_Y;
             TPuncte.Left = ALINIERE_X + DIMENSIUNE_PAS_X;
             this.Controls.Add(TPuncte);
+            */
 
             LData = new Label();
             LData.Width = LATIME_CONTROL;
@@ -109,12 +117,15 @@ namespace InterfataUtilizator_WindowsForms
             LData.TextAlign = ContentAlignment.MiddleCenter;
             this.Controls.Add( LData );
 
+            /* TextBoxData pentru citire definit prin cod
             TData = new TextBox();
             TData.Width = LATIME_CONTROL;
             TData.Top = ALINIERE_Y + DIMENSIUNE_PAS_Y;
             TData.Left = ALINIERE_X + 2 * DIMENSIUNE_PAS_X;
             this.Controls.Add(TData);
+            */
 
+            /* Butoane adauga si refresh declarate prin cod
             btnAdauga = new Button();
             btnAdauga.Width = LATIME_CONTROL;
             btnAdauga.Location = new Point(ALINIERE_X + 3 * DIMENSIUNE_PAS_X, ALINIERE_Y);
@@ -132,15 +143,17 @@ namespace InterfataUtilizator_WindowsForms
             btnRefresh.BackColor = Color.Aquamarine;
             btnRefresh.Click += OnButtonRefreshClick;
             this.Controls.Add( btnRefresh );
+            */
 
             NickError = new Label();
             DateError = new Label();
             PctError = new Label();
+            btnNext = new Button();
         }
 
         private void Form1_Load(Object sender, EventArgs e)
         {
-            AfisarePlayeri();
+            
         }
 
         private void OnButtonAdaugaClick(Object sender, EventArgs e)
@@ -151,12 +164,19 @@ namespace InterfataUtilizator_WindowsForms
             string errorMessage;
             if (ValidareInput(out errorMessage))
             {
-                Players player = new Players(TNickname.Text, TData.Text, Convert.ToInt32(TPuncte.Text));
+                Players player = new Players(textBoxNick.Text, textBoxData.Text, Convert.ToInt32(textBoxPct.Text));
                 player.idPlayer = idPlayeri;
                 adminPlayer.AddPlayer(player);
-
+                //stergere erori si continut introdus
                 DateError.Text = String.Empty;
+                PctError.Text = String.Empty;
+                NickError.Text = String.Empty;
                 LData.ForeColor = Color.Blue;
+                LNickname.ForeColor = Color.Blue;
+                LPuncte.ForeColor = Color.Blue;
+                textBoxNick.Text = String.Empty;
+                textBoxData.Text = String.Empty;
+                textBoxPct.Text = String.Empty;
             }
             else
             {
@@ -176,6 +196,7 @@ namespace InterfataUtilizator_WindowsForms
                     LNickname.ForeColor = Color.Red;
                     PctError.Text = String.Empty;
                     DateError.Text = String.Empty;
+                    textBoxNick.Text = String.Empty;
                 }
                 if (errorMessage == "13" || errorMessage == "23")
                 {
@@ -195,6 +216,7 @@ namespace InterfataUtilizator_WindowsForms
                     LNickname.ForeColor = Color.Blue;
                     NickError.Text = String.Empty;
                     DateError.Text = String.Empty;
+                    textBoxPct.Text = String.Empty;
                 }
                 if (errorMessage == "12" || errorMessage == "22")
                 {
@@ -214,48 +236,128 @@ namespace InterfataUtilizator_WindowsForms
                     LPuncte.ForeColor = Color.Blue;
                     NickError.Text = String.Empty;
                     PctError.Text = String.Empty;
+                    textBoxData.Text = String.Empty;
                 }
             }
         }
-        private void OnButtonRefreshClick(Object sender, EventArgs e)
+        /*private void OnButtonRefreshClick(Object sender, EventArgs e)
         {
             AfisarePlayeri();
-            TNickname.Text = String.Empty;
-            TPuncte.Text = String.Empty;
-            TData.Text = String.Empty;
+            textBoxNick.Text = String.Empty;
+            textBoxPct.Text = String.Empty;
+            textBoxData.Text = String.Empty;
+        }*/
+        private void OnButtonAfiseazaClick(Object sender, EventArgs e)
+        {
+            Players[] Player = adminPlayer.GetPlayeri(out int nrPlayeri);
+            this.Size = new Size( 1300 , 200 );
+            this.AutoSize = true;
+            AfisarePlayeri();
+            if (Screen.PrimaryScreen.Bounds.Height <= (nrPlayeri + 1) * DIMENSIUNE_PAS_Y + ALINIERE_Y)
+            {
+                btnNext.Text = "&Next";
+                btnNext.Width = 85;
+                btnNext.Height = 23;
+                btnNext.Location = new Point(ALINIERE_X + 3 * DIMENSIUNE_PAS_X, ALINIERE_Y + 2 * DIMENSIUNE_PAS_Y);
+                btnNext.Font = new Font("Arial", 9, FontStyle.Bold);
+                btnNext.BackColor = Color.Aquamarine;
+                btnNext.Click += OnButtonNextClick;
+                this.Controls.Add(btnNext);
+            }
+                this.CenterToScreen();
+        }
+        private void OnFormClick(Object sender, EventArgs e)
+        {
+            this.AutoSize = false;
+            this.Size = new Size(730, 155);
+            this.CenterToScreen();
+            // stergerea Label-urilor
+            this.Controls.Remove(lblTitlu);
+            Players[] Player = adminPlayer.GetPlayeri(out int nrPlayeri);
+            for (int i = 0; i < nrPlayeri; i++)
+            {
+                this.Controls.Remove(lblTablou[0, i]);
+                this.Controls.Remove(lblTablou[1, i]);
+                this.Controls.Remove(lblTablou[2, i]);
+            }
+            this.Controls.Remove(btnNext);
+        }
+        private void OnButtonNextClick(Object sender, EventArgs e)
+        {
+            Players[] Player = adminPlayer.GetPlayeri(out int nrPlayeri);
+            int afisStop = (Screen.PrimaryScreen.Bounds.Height - ALINIERE_Y) / DIMENSIUNE_PAS_Y - 3;
+            this.Controls.Remove(lblTitlu);
+            for (int i = 0; i < afisStop; i++)
+            {
+                this.Controls.Remove(lblTablou[0, i]);
+                this.Controls.Remove(lblTablou[1, i]);
+                this.Controls.Remove(lblTablou[2, i]);
+            }
+            for (int i = afisStop; i < nrPlayeri; i++)
+            {
+                lblTablou[0, i] = new Label();
+                lblTablou[0, i].Text = Player[i].Nickname;
+                lblTablou[0, i].Font = new Font("Arial", 9, FontStyle.Bold);
+                lblTablou[0, i].Width = LATIME_CONTROL;
+                lblTablou[0, i].Top = (i - afisStop ) * DIMENSIUNE_PAS_Y + ALINIERE_Y;
+                lblTablou[0, i].Left = 4 * DIMENSIUNE_PAS_X + ALINIERE_X;
+                lblTablou[0, i].BackColor = Color.LightGreen;
+                lblTablou[0, i].TextAlign = ContentAlignment.MiddleCenter;
+                this.Controls.Add(lblTablou[0, i]);
+
+                lblTablou[1, i] = new Label();
+                lblTablou[1, i].Text = Player[i].Punctaj.ToString();
+                lblTablou[1, i].Font = new Font("Cambria", 9, FontStyle.Bold);
+                lblTablou[1, i].Width = LATIME_CONTROL;
+                lblTablou[1, i].Top = (i - afisStop ) * DIMENSIUNE_PAS_Y + ALINIERE_Y;
+                lblTablou[1, i].Left = 5 * DIMENSIUNE_PAS_X + ALINIERE_X;
+                lblTablou[1, i].BackColor = Color.LightGreen;
+                lblTablou[1, i].TextAlign = ContentAlignment.MiddleCenter;
+                this.Controls.Add(lblTablou[1, i]);
+
+                lblTablou[2, i] = new Label();
+                lblTablou[2, i].Text = Player[i].Data;
+                lblTablou[2, i].Font = new Font("Cambria", 9, FontStyle.Bold);
+                lblTablou[2, i].Width = LATIME_CONTROL;
+                lblTablou[2, i].Top = (i - afisStop ) * DIMENSIUNE_PAS_Y + ALINIERE_Y;
+                lblTablou[2, i].Left = 6 * DIMENSIUNE_PAS_X + ALINIERE_X;
+                lblTablou[2, i].BackColor = Color.LightGreen;
+                lblTablou[2, i].TextAlign = ContentAlignment.MiddleCenter;
+                this.Controls.Add(lblTablou[2, i]);
+            }
         }
         public bool ValidareInput(out string errorMessage)
         {
-            if (TNickname.Text.Length == 0)
+            if (textBoxNick.Text.Length == 0)
             {
                 errorMessage = "11";
                 return false;
             }
-            if (TNickname.Text.Length > LUNGIME_NICKNAME)
+            if (textBoxNick.Text.Length > LUNGIME_NICKNAME)
             {
                 errorMessage = "21";
                 return false;
             }
-            if (TPuncte.Text.Length == 0)
+            if (textBoxPct.Text.Length == 0)
             {
                 errorMessage = "13";
                 return false;
             }
-            if (!Int32.TryParse(TPuncte.Text, out int result))
+            if (!Int32.TryParse(textBoxPct.Text, out int result))
             {
                 errorMessage = "23";
                 return false;
             }
-            if (TData.Text.Length == 0)
+            if (textBoxData.Text.Length == 0)
             {
                 errorMessage = "12";
                 return false;
             }
             bool nr = true;
-            for (int i = 0; i < TData.Text.Length && i != 2 && i != 5; i++)
-                if (TData.Text[i] - '0' < 0 || TData.Text[i] - '0' > 9)
+            for (int i = 0; i < textBoxData.Text.Length && i != 2 && i != 5; i++)
+                if (textBoxData.Text[i] - '0' < 0 || textBoxData.Text[i] - '0' > 9)
                     nr = false;
-            if (TData.Text.Length != 10 || TData.Text[2] != '.' || TData.Text[5] != '.' || !nr)
+            if (textBoxData.Text.Length != 10 || textBoxData.Text[2] != '.' || textBoxData.Text[5] != '.' || !nr)
             {
                 errorMessage = "22";
                 return false;
@@ -271,8 +373,8 @@ namespace InterfataUtilizator_WindowsForms
 
             lblTitlu = new Label();
             lblTitlu.Width = 400;
-            lblTitlu.Left = 100;
-            lblTitlu.Top = 3 * DIMENSIUNE_PAS_Y + ALINIERE_Y;
+            lblTitlu.Left = 4 * DIMENSIUNE_PAS_X + 100;
+            lblTitlu.Top = ALINIERE_Y;
             lblTitlu.Text = "Playeri";
             lblTitlu.BackColor = Color.LightGreen;
             lblTitlu.TextAlign = ContentAlignment.MiddleCenter;
@@ -280,36 +382,75 @@ namespace InterfataUtilizator_WindowsForms
 
             for (int i = 0; i < nrPlayeri; i++)
             {
-                lblTablou[0, i] = new Label();
-                lblTablou[0, i].Text = Player[i].Nickname;
-                lblTablou[0, i].Font = new Font("Arial", 9, FontStyle.Bold);
-                lblTablou[0, i].Width = LATIME_CONTROL;
-                lblTablou[0, i].Top = (i + 4) * DIMENSIUNE_PAS_Y + ALINIERE_Y;
-                lblTablou[0, i].Left = ALINIERE_X;
-                lblTablou[0, i].BackColor = Color.LightGreen;
-                lblTablou[0, i].TextAlign = ContentAlignment.MiddleCenter;
-                this.Controls.Add(lblTablou[0, i]);
+                    lblTablou[0, i] = new Label();
+                    lblTablou[0, i].Text = Player[i].Nickname;
+                    lblTablou[0, i].Font = new Font("Arial", 9, FontStyle.Bold);
+                    lblTablou[0, i].Width = LATIME_CONTROL;
+                    lblTablou[0, i].Top = (i + 1) * DIMENSIUNE_PAS_Y + ALINIERE_Y;
+                    lblTablou[0, i].Left = 4 * DIMENSIUNE_PAS_X + ALINIERE_X;
+                    lblTablou[0, i].BackColor = Color.LightGreen;
+                    lblTablou[0, i].TextAlign = ContentAlignment.MiddleCenter;
+                    this.Controls.Add(lblTablou[0, i]);
 
-                lblTablou[1, i] = new Label();
-                lblTablou[1, i].Text = Player[i].Punctaj.ToString();
-                lblTablou[1, i].Font = new Font("Cambria", 9, FontStyle.Bold);
-                lblTablou[1, i].Width = LATIME_CONTROL;
-                lblTablou[1, i].Top = (i + 4) * DIMENSIUNE_PAS_Y + ALINIERE_Y;
-                lblTablou[1, i].Left = DIMENSIUNE_PAS_X + ALINIERE_X;
-                lblTablou[1, i].BackColor = Color.LightGreen;
-                lblTablou[1, i].TextAlign = ContentAlignment.MiddleCenter;
-                this.Controls.Add(lblTablou[1, i]);
+                    lblTablou[1, i] = new Label();
+                    lblTablou[1, i].Text = Player[i].Punctaj.ToString();
+                    lblTablou[1, i].Font = new Font("Cambria", 9, FontStyle.Bold);
+                    lblTablou[1, i].Width = LATIME_CONTROL;
+                    lblTablou[1, i].Top = (i + 1) * DIMENSIUNE_PAS_Y + ALINIERE_Y;
+                    lblTablou[1, i].Left = 5 * DIMENSIUNE_PAS_X + ALINIERE_X;
+                    lblTablou[1, i].BackColor = Color.LightGreen;
+                    lblTablou[1, i].TextAlign = ContentAlignment.MiddleCenter;
+                    this.Controls.Add(lblTablou[1, i]);
 
-                lblTablou[2, i] = new Label();
-                lblTablou[2, i].Text = Player[i].Data;
-                lblTablou[2, i].Font = new Font("Cambria", 9, FontStyle.Bold);
-                lblTablou[2, i].Width = LATIME_CONTROL;
-                lblTablou[2, i].Top = (i + 4) * DIMENSIUNE_PAS_Y + ALINIERE_Y;
-                lblTablou[2, i].Left = 2 * DIMENSIUNE_PAS_X + ALINIERE_X;
-                lblTablou[2, i].BackColor = Color.LightGreen;
-                lblTablou[2, i].TextAlign = ContentAlignment.MiddleCenter;
-                this.Controls.Add(lblTablou[2, i]);
+                    lblTablou[2, i] = new Label();
+                    lblTablou[2, i].Text = Player[i].Data;
+                    lblTablou[2, i].Font = new Font("Cambria", 9, FontStyle.Bold);
+                    lblTablou[2, i].Width = LATIME_CONTROL;
+                    lblTablou[2, i].Top = (i + 1) * DIMENSIUNE_PAS_Y + ALINIERE_Y;
+                    lblTablou[2, i].Left = 6 * DIMENSIUNE_PAS_X + ALINIERE_X;
+                    lblTablou[2, i].BackColor = Color.LightGreen;
+                    lblTablou[2, i].TextAlign = ContentAlignment.MiddleCenter;
+                    this.Controls.Add(lblTablou[2, i]);
             }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void GotFocusNick(Object sender, EventArgs e)
+        {
+            textBoxNick.BackColor = Color.BlueViolet;
+        }
+        private void LostFocusNick(Object sender, EventArgs e)
+        {
+            textBoxNick.BackColor = Color.White;
+        }
+        private void GotFocusPct(Object sender, EventArgs e)
+        {
+            textBoxPct.BackColor = Color.BlueViolet;
+        }
+        private void LostFocusPct(Object sender, EventArgs e)
+        {
+            textBoxPct.BackColor = Color.White;
+        }
+        private void GotFocusDate(Object sender, EventArgs e)
+        {
+            textBoxData.BackColor = Color.BlueViolet;
+        }
+        private void LostFocusDate(Object sender, EventArgs e)
+        {
+            textBoxData.BackColor = Color.White;
         }
     }
 }
